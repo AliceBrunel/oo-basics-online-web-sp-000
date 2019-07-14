06fbb733fa2a3c670b282e39ba953e7698a5194e
Alice Brunel <alc.brunel@gmail.com>
2019-07-14 18:07:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,12 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
  
   def initialize(brand)
-    @brand = title
+    @brand = brand
   end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def cooble
   end
  
 end
